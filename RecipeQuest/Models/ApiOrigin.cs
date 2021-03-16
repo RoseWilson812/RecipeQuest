84c6694b3afa7a29d3db8c8fe256655b4c5b66b0
Rose Wilson <rosewilson812@yahoo.com>
2021-03-16 13:20:58 -0500

    Added search-all-page

---
+++ RecipeQuest/Models/ApiOrigin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeQuest.Models
+{
+    public class ApiOrigin
+    {
+        public string strArea { get; set; }
+    }
+}
