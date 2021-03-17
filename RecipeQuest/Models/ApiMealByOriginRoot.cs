fef0101dba44c581d040aaf954b7638cf168c452
Rose Wilson <rosewilson812@yahoo.com>
2021-03-17 12:01:30 -0500

    Added  display page for meal by origin

---
+++ RecipeQuest/Models/ApiMealByOriginRoot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeQuest.Models
+{
+    public class ApiMealByOriginRoot
+    {
+        public ApiMealByOrigin[] meals { get; set; }
+    }
+}
---
+++ RecipeQuest/Models/MealByOrigin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeQuest.Models
+{
+    public class MealByOrigin
+    {
+        public int Id { get; set; }
+        public string StrMeal { get; set; }
+        public string StrMealThumb { get; set; }
+        public string IdMeal { get; set; }
+
+
+        public MealByOrigin()
+        {
+        }
+        public MealByOrigin(string meal, string mealImage, string mealId)
+        {
+            StrMeal = meal;
+            StrMealThumb = mealImage;
+            IdMeal = mealId;
+
+        }
+    }
+}
